1adc9074e2071f5ba96fa949266b14a1ddb0fd67
MulubahzumuKSipor <156727431+MulubahzumuKSipor@users.noreply.github.com>
2024-11-09 10:47:03 +0000

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Course grade in numbers? ");
+        string score = Console.ReadLine();
+        int finalScore = int.Parse(score);
+        if (finalScore >= 93)
+        {
+            Console.WriteLine("A");
+        }
+        else if (finalScore >= 90)
+        {
+            Console.WriteLine("A-");
+        }
+        else if (finalScore >= 84)
+        {
+            Console.WriteLine("B+");
+        }
+        else if (finalScore >= 80)
+        {
+            Console.WriteLine("B-");
+        }
+        else if (finalScore >= 74)
+        {
+            Console.WriteLine("C+");
+        }
+        else if (finalScore >= 70)
+        {
+            Console.WriteLine("C-");
+        }
+        else if (finalScore >= 64)
+        {
+            Console.WriteLine("D+");
+        }
+        else if (finalScore >= 60)
+        {
+            Console.WriteLine("D-");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
