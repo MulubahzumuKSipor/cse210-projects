2e4cea5477d6217f68af7ba42b4c1bce3c01777c
MulubahzumuKSipor <156727431+MulubahzumuKSipor@users.noreply.github.com>
2024-11-09 15:29:11 +0000

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+        int number = -1;
+
+
+
+        while (number != magicNumber)
+        {
+            Console.Write("What is the magic number? ");
+            string word = Console.ReadLine();
+            number = int.Parse(word);
+
+            if (number > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.Write($"With {magicNumber} being the magic number, You've passed!");
+            }
+
+
+        }
     }
 }
